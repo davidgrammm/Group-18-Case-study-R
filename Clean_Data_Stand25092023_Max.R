041afd0fadc1b8102595999d82e536f309c4272b
Max Florian Henning <max.henning26@gmail.com>
2023-08-25 16:14:10 +0200

    Max hat das aktuelle Dataset erzeugt

--- Clean_Data_Stand25092023_Max.R
+++ Clean_Data_Stand25092023_Max.R
@@ -71,31 +71,57 @@ Bestandteile_Fahrzeuge <- rbind(Bestandteile_Fahrzeug_Typ11,Bestandteile_Fahrzeu
 Bestandteile_Komponenten <- inner_join(Komponenten_fertig,Bestandteile_Fahrzeuge, by="ID_Schaltung")%>% 
   select(ID_T21, ID_Schaltung, Fehlerhaft_T21, Fehlerhaft_T21_Datum, Fehlerhaft_Schaltung, Fehlerhaft_Schaltung_Datum, ID_Fahrzeug)
 
+# Nun kann das Dataset mit den Fahrzeugdaten gejoined werden. Hier ist aber nur Fehlerfhaft
+# und Fehlerhaft Datum relevant. Das Produktionsdatum spielt keine Rolle, der Rest der 
+# Informationen ist in der ID enthalten
+
+Fahrzeuge_OEM1_Typ11 <- select(Fahrzeuge_OEM1_Typ11, ID_Fahrzeug, Fehlerhaft, Fehlerhaft_Datum)
+Fahrzeuge_OEM1_Typ12 <- select(Fahrzeuge_OEM1_Typ12, ID_Fahrzeug, Fehlerhaft, Fehlerhaft_Datum)
+Fahrzeuge_OEM2_Typ21 <- select(Fahrzeuge_OEM2_Typ21, ID_Fahrzeug, Fehlerhaft, Fehlerhaft_Datum)
+Fahrzeuge_OEM2_Typ22 <- select(Fahrzeuge_OEM2_Typ22, ID_Fahrzeug, Fehlerhaft, Fehlerhaft_Datum)
+
+
+Bestandteile_Komponenten_Fahrzeuge <- Bestandteile_Komponenten %>%
+  left_join(rbind(Fahrzeuge_OEM1_Typ11,Fahrzeuge_OEM1_Typ12,Fahrzeuge_OEM2_Typ21,Fahrzeuge_OEM2_Typ22), by = "ID_Fahrzeug") 
+
+Bestandteile_Komponenten_Fahrzeuge <- Bestandteile_Komponenten_Fahrzeuge %>%  
+  rename(Fehlerhaft_Fahrzeug = Fehlerhaft)%>% 
+  rename(Fehlerhaft_Fahrzeug_Datum = Fehlerhaft_Datum)
+  
+
 # Um jedem Fahrzeug die Zulassung zuzuordnen, muss IDNummer in ID_Fahrzeug umbenannt werden
 Zulassungen_column_renamed <- Zulassungen %>% 
   rename(ID_Fahrzeug = IDNummer)
 
 # jedem Fahrzeug werden die Zulassungsdaten zugeordnet
-Zulassungen_Bestandteile_Komponenten <- inner_join(Bestandteile_Komponenten,Zulassungen_column_renamed, by="ID_Fahrzeug")%>% 
-  select(ID_T21, ID_Schaltung, Fehlerhaft_T21, Fehlerhaft_T21_Datum, Fehlerhaft_Schaltung, Fehlerhaft_Schaltung_Datum, ID_Fahrzeug, Gemeinde=Gemeinden, Zulassung)
+Zulassungen_Bestandteile_Komponenten_Fahrzeuge <- inner_join(Bestandteile_Komponenten_Fahrzeuge,Zulassungen_column_renamed, by="ID_Fahrzeug") %>%
+  select(ID_T21, ID_Schaltung, Fehlerhaft_T21, Fehlerhaft_T21_Datum, Fehlerhaft_Schaltung, Fehlerhaft_Schaltung_Datum, ID_Fahrzeug,Fehlerhaft_Fahrzeug,Fehlerhaft_Fahrzeug_Datum, Gemeinde=Gemeinden, Zulassung)
 
 # jedem Fahrzeug inklusive Zulassung werden die Geodaten dieser Zulassung zugeordnet
-Geodaten_Zulassung_Bestandteile_Komponenten <- left_join(Zulassungen_Bestandteile_Komponenten, Geodaten, by="Gemeinde") %>% 
-  select(ID_T21, ID_Schaltung, Fehlerhaft_T21, Fehlerhaft_T21_Datum, Fehlerhaft_Schaltung, Fehlerhaft_Schaltung_Datum, ID_Fahrzeug, Gemeinde, Zulassung, Postleitzahl, Laengengrad, Breitengrad)
+Geodaten_Zulassung_Bestandteile_Komponenten_Fahrzeuge <- left_join(Zulassungen_Bestandteile_Komponenten_Fahrzeuge, Geodaten, by="Gemeinde") %>% 
+  select(ID_T21, ID_Schaltung, Fehlerhaft_T21, Fehlerhaft_T21_Datum, Fehlerhaft_Schaltung, Fehlerhaft_Schaltung_Datum, ID_Fahrzeug,Fehlerhaft_Fahrzeug,Fehlerhaft_Fahrzeug_Datum, Gemeinde, Zulassung, Postleitzahl, Laengengrad, Breitengrad)
 
+
+#FÜR DIE DOKU MUSS HIER NOCH EIN BEFEHL REIN, MIT DEM RAUSGEFUNDEN WURDE, DASS FÜR SEEG DIE DATEN FEHLEN
 # Für die Gemeinde SEEG fehlen die Geodaten, diese müssen deshalb nachgepflegt werden
-Geodaten_Zulassung_Bestandteile_Komponenten$Laengengrad[Geodaten_Zulassung_Bestandteile_Komponenten$Gemeinde == "SEEG"] <- "10.6104157" #Laengengrad nachpflegen
-Geodaten_Zulassung_Bestandteile_Komponenten$Breitengrad[Geodaten_Zulassung_Bestandteile_Komponenten$Gemeinde == "SEEG"] <- "47.6542215" #Breitengrad nachpflegen
-Geodaten_Zulassung_Bestandteile_Komponenten$Postleitzahl[Geodaten_Zulassung_Bestandteile_Komponenten$Gemeinde == "SEEG"] <- "87637"     #PLZ nachpflegen
+Geodaten_Zulassung_Bestandteile_Komponenten_Fahrzeuge $Laengengrad[Geodaten_Zulassung_Bestandteile_Komponenten$Gemeinde == "SEEG"] <- "10.6104157" #Laengengrad nachpflegen
+Geodaten_Zulassung_Bestandteile_Komponenten_Fahrzeuge $Breitengrad[Geodaten_Zulassung_Bestandteile_Komponenten$Gemeinde == "SEEG"] <- "47.6542215" #Breitengrad nachpflegen
+Geodaten_Zulassung_Bestandteile_Komponenten_Fahrzeuge $Postleitzahl[Geodaten_Zulassung_Bestandteile_Komponenten$Gemeinde == "SEEG"] <- "87637"     #PLZ nachpflegen
 
 # Prüfen, in wievielen verschiedenen Gemeinden betroffene Autos zugelassen sind
 # So kann überprüft werden, ob potentiell überhaupt genug Geodaten zur Verfügung stehen
-anz_unique_gemeinden <- length(unique(Geodaten_Zulassung_Bestandteile_Komponenten$Gemeinde))
+anz_unique_gemeinden <- length(unique(Geodaten_Zulassung_Bestandteile_Komponenten_Fahrzeuge $Gemeinde))
 print(anz_unique_gemeinden)
 
 # Tatsächlich stehen ausreichend Geodaten zur Verfügung, im nächsten Schritt muss überprüft werden,
 # ob jeder Zulassung auch ein Langen und Breitengrad zugeteilt werden kann
 
+print(paste("Zulassungen ohne zugehörige PostleitzahL:",sum(is.na(Geodaten_Zulassung_Bestandteile_Komponenten_Fahrzeuge $Postleitzahl))))
+
+#TO DO:
+# Anstatt immmer 10 Zeilen zu selecten, vlt einfach 2 Zeilen rauswerfen (man kann auch irgendwie select(-c(...))) machen
+
+# UNTERE TO DOs sind erledigt
 # TO Do: Fahrzeuglisten OEM2 Produktionsdatum aus den zwei Spalten erzeugen, um Join mit OEM1 Fahrzeugen zu ermöglichen
 # Das Dataset Geodaten_Zulassung_Bestandteile_Komponenten mit Fahrzeuge joinen, um Daten zu erhalten, ob Fahrzeuge kaputt sind
 # AAABER evtl. unnötig, wenn Produktionsdatum immer Zulassungsdatum -1: Dann kann man Produktionsdatum komplett ignorieren
